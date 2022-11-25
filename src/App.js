f0c11c9a8249d0ec8e6facb5ff0110c879cfd225
Rémi Raumain <remiraumain@Mac-mini-de-Remi.local>
2022-11-25 12:51:54 +0100

    Setup for r3f

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Perf } from "r3f-perf";
+import { useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const cube = useRef()
+
+	useFrame((state, delta) => 
+	{
+		cube.current.rotation.y += delta
+	})
+
+	return <>
+		<Perf position="top-left" />
+
+		<OrbitControls makeDefault />
+
+		<directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+		<ambientLight intensity={0.5} />
+
+		<mesh ref={cube} castShadow >
+			<boxGeometry />
+			<meshStandardMaterial color="mediumpurple" />
+		</mesh>
+
+		<mesh receiveShadow position-y={- 1.25}>
+			<boxGeometry args={[10, 0.5, 10]} />
+			<meshStandardMaterial color="greenyellow" />
+		</mesh>
+	</>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Canvas } from '@react-three/fiber';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Canvas
+			shadows
+			camera={{
+				fov: 45,
+				near: 0.1,
+				far: 200,
+				position: [4, 2, 6]
+			}}
+		>
+			<App />
+		</Canvas>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
